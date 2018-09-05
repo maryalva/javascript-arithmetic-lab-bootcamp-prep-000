55a99d23b5eef6871910cc6f7f2fd83764792fc9
Mary A <mary.alva@icloud.com>
2018-09-05 20:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,58 +2,38 @@ function add(x, y) {
     return x + y;
 }
 
-add(2, 7);
-
 
 function subtract(x, y) {
   return x - y;
 }
-
-subtract(7,2);
 
 
 function multiply(x, y) {
   return x * y;
 }
 
-multiply(2,7);
-
 
 function divide(x, y) {
   return x / y;
 }
-
-divide(14,2)
 
 
 function inc(n) {
   return n + 1;
 }
 
-inc(5)
-
 
 function dec(n) {
   return n - 1;
 }
-
-dec(7)
 
 
 function makeInt(n) {
   return (parseInt(n, 10));
 }
 
-makeInt('2');
-
-makeInt('This');
-
 
 function preserveDecimal(n) {
   return (parseFloat(n))
 }
-
-preserveDecimal('2.22')
-
-preserveDecimal('That')
 
