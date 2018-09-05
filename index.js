b28c9a242d875860edfb932492ea9d86c4bb4e0c
Mary A <mary.alva@icloud.com>
2018-09-05 20:28:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(x, y) {
+    return x + y;
+}
+
+add(2, 7);
+
+
+function subtract(x, y) {
+  return x - y;
+}
+
+subtract(7,2);
+
+
+function multiply(x, y) {
+  return x * y;
+}
+
+multiply(2,7);
+
+
+function divide(x, y) {
+  return x / y;
+}
+
+divide(14,2)
+
+var n = 5;
+
+function makeInt(n, 10) }
+  return n;
+}
+
+makeInt(25.52,10);
+
+
+
